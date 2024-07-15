0f248331a0b1d4288124ebad6b7a400a884f60a0
jackcamerontaylor <jackgtdwwq@icloud.com>
2024-07-15 22:16:23 +0100

    implemented huggingface transformer model - data still needs to be properly cleaned.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 
 from src.collect_tweets import main
+from src.clean_data import clean_data
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    tweets = asyncio.run(main())
+    clean_data(tweets)
